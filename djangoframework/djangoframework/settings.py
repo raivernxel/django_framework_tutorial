433b748c41a31a6e4fbd5330992991b86f0117cc
raivernxel <vinsumalinog@gmail.com>
2024-09-05 17:45:51 +0800

    Lesson-3: -Added Post model to migrate on the database. -Added configurations in settings for the MySQL Database.
    -Migrates the model on the database using this terminal:
     py manage.py makemigrations
       to make the Post Model ready for migration on the database.
     py manage.py migrate
       to migrate the Post Model on the database.

--- djangoframework/djangoframework/settings.py
+++ djangoframework/djangoframework/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'djangoframework.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Use 'mysql' for mysqlclient or 'django.db.backends.mysql' for PyMySQL
+        'NAME': 'django_tutorial',             # Replace with your database name
+        'USER': 'root',                        # Replace with your MySQL username
+        'PASSWORD': 'MCKRaivern0304!',         # Replace with your MySQL password
+        'HOST': 'localhost',                   # Or your MySQL host if it's different
+        'PORT': '3306',                        # Default MySQL port
     }
 }
 
